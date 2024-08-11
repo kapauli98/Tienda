66327a1c83df72030a5e8171b57513b897e59fd7
kapau <kapau@Kapauli98>
2024-08-10 21:38:06 -0600

    semana9

--- src/main/java/com/tienda/dao/ProductoDao.java
+++ src/main/java/com/tienda/dao/ProductoDao.java
@@ -1,9 +1,12 @@
-
 package com.tienda.dao;
 
 import com.tienda.domain.Producto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoDao extends JpaRepository<Producto, Long>{
-    
+public interface ProductoDao extends JpaRepository<Producto, Long> {
+    //Ejemplo de método utilizando Métodos de Query
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    public List<Producto> findByDescripcionContainingIgnoreCase (String texto);
+
 }
